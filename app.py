a818d1c6c71d906118891a0a22d0f406cf8660fd
kimsemin_ <lies123qqqq@gmail.com>
2025-09-17 21:37:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -55,14 +55,14 @@ class 점메추App(App):
         #self.bc = ['떡볶이', '순대', '튀김', '김밥', '어묵', '라볶이', '쫄면', '라면', '잔치국수', '충무김밥', '유부초밥', '볶음밥', '김치볶음밥', '튀김만두', '삶은 계란', '핫도그', '컵밥', '꼬마김밥', '떡꼬치', '소떡소떡', '닭꼬치', '콜팝']
         #self.en = ['햄버거', '스테이크', '핫도그', '피자', '치킨', '바비큐 립', '베이글', '샌드위치', '팬케이크', '와플', '프렌치 토스트', '오믈렛', '베이컨 & 에그', '프렌치프라이', '어니언링', '콜슬로', '머핀', '스모어', '클램 차우더', '검보', '타코', '엔칠라다', '잠발라야', '미트볼', '맥앤치즈']
         #self.all =['김치찌개','된장찌개','순두부찌개','부대찌개','갈비탕','설렁탕','순대국밥','콩나물국밥','뼈해장국','미역국','감자탕','비빔밥','김치볶음밥','돌솥비빔밥','제육덮밥','오징어덮밥','뚝배기제육','잔치국수','김치말이국수','칼국수','쌀국수','평양냉면','물냉면','비빔냉면','삼겹살','돼지갈비','소불고기','제육볶음','닭볶음탕','족발','장충동보쌈','떡볶이','어묵','찜닭','아구찜','간장게장','보리밥','쭈구미볶음','초밥','회덮밥','참치회','연어회','우동','라멘','돈까스','가츠동','오야코동','규동','텐동','카레라이스','타코야끼','오코노미야끼','짜장면','볶음짬뽕','간짜장','쟁반짜장','울면','기스면','짬뽕','탕수육','마파두부덮밥','양장피','팔보채','깐풍기','유산슬','고추잡채밥','동파육','라조기','유린기','양장피','팔보채','고추잡채','크림새우','칠리새우','마라탕','마라샹궈','훠궈','꿔바로우','즈란심관(양고기 볶음)','군만두','물만두','떡볶이', '순대', '튀김', '김밥', '어묵', '라볶이', '쫄면', '라면', '잔치국수', '충무김밥', '유부초밥', '볶음밥', '김치볶음밥', '튀김만두', '삶은 계란', '핫도그', '컵밥', '꼬마김밥', '떡꼬치', '소떡소떡', '닭꼬치', '콜팝','햄버거', '스테이크', '핫도그', '피자', '치킨', '바비큐 립', '베이글', '샌드위치', '팬케이크', '와플', '프렌치 토스트', '오믈렛', '베이컨 & 에그', '프렌치프라이', '어니언링', '콜슬로', '머핀', '스모어', '클램 차우더', '검보', '타코', '엔칠라다', '잠발라야', '미트볼', '맥앤치즈']
-        self.menu_label = Label(text='오늘 점심을 뭐 먹을지 고민된다면? 점메추 버튼 한번으로 점심먹기!', font_name='BMJUA_ttf.ttf', font_size=30)
-        button = Button(text='랜덤 점메추하기',font_name='BMJUA_ttf.ttf',size_hint_y=None, height=100 , font_size=50)
+        self.menu_label = Label(text='오늘의 운세 뽑기! 오늘도 힘차게 아자아자!', font_name='BMJUA_ttf.ttf', font_size=30)
+        button = Button(text='지금 운세뽑기기',font_name='BMJUA_ttf.ttf',size_hint_y=None, height=100 , font_size=50)
         #button1 = Button(text='한식 점메추하기', font_name='BMJUA_ttf.ttf', size_hint_y=None, height=50, font_size=40)
         #button2 = Button(text='일식 점메추하기', font_name='BMJUA_ttf.ttf', size_hint_y=None, height=50, font_size=40)
         #button3 = Button(text='중식 점메추하기', font_name='BMJUA_ttf.ttf', size_hint_y=None, height=50, font_size=40)
         #button4 = Button(text='미식 점메추하기', font_name='BMJUA_ttf.ttf', size_hint_y=None, height=50, font_size=40)
         #button5 = Button(text='분식 점메추하기', font_name='BMJUA_ttf.ttf', size_hint_y=None, height=50, font_size=40)
-        button.bind(on_press=self.all1)
+        button.bind(on_press=self.ko1)
         #button1.bind(on_press=self.ko1)
         #button2.bind(on_press=self.jp1)
         #button3.bind(on_press=self.cn1)
