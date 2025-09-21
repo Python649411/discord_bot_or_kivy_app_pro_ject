4dbf0aa71331530242d1c3aee02d83c1119635da
ko1098-ops <ko1098@naver.com>
2025-09-21 13:39:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ class 운세뽑기App(App):
         layout = BoxLayout(orientation="vertical")
         
 
-        self.ko = ["오늘은 여친이 생기실거에요","오늘은 부자가 되실거예요","오늘은 날씨가 안좋아서 비를 맞을거에요","오늘은 포경수술을 할거예요","오늘은 학교에서 좋은일이 생겨요","오늘은 좋은것을 선택해요"]
+        self.ko = ["오늘은 여친이 생기실거에요","오늘은 부자가 되실거예요","오늘은 날씨가 안좋아서 비를 맞을거에요","오늘은 포경수술을 할거예요","오늘은 학교에서 좋은일이 생겨요","오늘은 좋은것을 선택해요","오늘은 안아프지 않아요","나 오늘야돈까스 먹을꺼야"]
        
         self.menu_label = Label(text='오늘의 운세 뽑기! 오늘도 힘차게 아자아자!', font_name='BMJUA_ttf.ttf', font_size=30)
         button = Button(text='지금 운세뽑기',font_name='BMJUA_ttf.ttf',size_hint_y=None, height=100 , font_size=50)
@@ -37,7 +37,7 @@ class 운세뽑기App(App):
         layout.add_widget(self.menu_label)
         layout.add_widget(image1)
         layout.add_widget(button)
-        
+
         return layout
     
         
